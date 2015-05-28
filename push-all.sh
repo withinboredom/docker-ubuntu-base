3d9e08b0acc4529cdfecd3c6b67fd8e9e283a0af
Rob Landers <landers.robert@gmail.com>
2015-05-28 00:20:17 -0400

    Updated s6

--- push-all.sh
+++ push-all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#ubuntu-base/push.sh
-#consul/push.sh
-#consul-ui/push.sh
-#consul-agent/push.sh
-#rethinkdb/push.sh
+ubuntu-base/push.sh
+consul/push.sh
+consul-ui/push.sh
+consul-agent/push.sh
+rethinkdb/push.sh
 blue-volume/push.sh
